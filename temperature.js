d16b0cf46c126c6346b2a6e4b12d4e2d177c7f06
sachin jamthe <>
2018-04-10 20:46:06 -0500

    First commit

---
+++ temperature.js
@@ -0,0 +1,11 @@
+//Code for celsius to farenheit
+var temperatureInCelsius = 40;
+var celsiusToFarenheit = (temperatureInCelsius * 9)/5 + 32;
+//Log celsius to farenheit to console
+console.log('Temperature of ' + temperatureInCelsius + ' celsius, will be ' + celsiusToFarenheit + ' farenheit');
+
+//code for farenheit to celsius
+var temperatureInFarenheit = 104;
+var farenheitToCelsius = ((temperatureInFarenheit - 32)* 5)/9;
+//Log farenheit to celsius to console
+console.log('Temperature of ' + temperatureInFarenheit + ' farenheit, will be ' + farenheitToCelsius + ' celsius');
